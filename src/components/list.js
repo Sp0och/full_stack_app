b8a1688a61270eee720cd20e49524de5a20993ee
Spooch <fierz.lasse@gmail.com>
2022-06-05 08:50:57 +0200

    started react list functionality

--- src/components/list.js
+++ src/components/list.js
@@ -3,23 +3,24 @@ import '../css/list.css'
 
 
 
-function Button(removeItem) {
+function Button({removeItem}) {
   return (
-    <a href="#"><i class="fa-solid fa-xmark"></i></a>
+    <a href="#" onClick={removeItem}><i class="fa-solid fa-xmark"></i></a>
   )
 }
 
 
 function ShoppingList() {
 
-    
+    const removeItem = (e) => {
+        e.preventDefault();
+        console.log("remove");
+    }
 
   return (
     <div className='list-container'>
         <ul>
-            <li><p>Hello World</p><Button/></li>
-            <li><p>Hello World</p><Button/></li>
-            <li><p>Hello World</p><Button/></li>
+            <li><p>Hello World</p><Button onClick={()=> removeItem()}/></li>
             <li><p>Hello World</p><Button/></li>
             <li><p>Hello World</p><Button/></li>
             <li><p>Hello World</p><Button/></li>
